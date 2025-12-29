68edbf2bf5fbe595bc49b6f680db9f697dc608fd
Mark2754 <scharonowmark@gmail.com>
2025-12-29 02:22:43 +0100

    v2 - Everything
    Added multiple new .ipt and .iam hardware CAD files, updated several existing hardware assemblies, and removed obsolete STEP/STP files. Also added new KiCad PCB and schematic files, backup archives, and documentation images. Old version files were updated to reflect the latest hardware changes.

---
+++ software/main/encoder_rd.ino
@@ -0,0 +1,98 @@
+/*
+ * AMT22_Multiurn_SPI_Sample_Code_Uno.ino
+ * Company: CUI Devices
+ * Author: Jason Kelly, Damon Tarry
+ * Version: 2.0.2.0
+ * Date: July 18, 2023
+ *
+ * This sample code can be used with the Arduino Uno to control the AMT22 encoder.
+ * It uses SPI to control the encoder and the the Arduino UART to report back to the PC
+ * via the Arduino Serial Monitor.
+ * For more information or assistance contact CUI Devices for support.
+ *
+ * After uploading code to Arduino Uno open the open the Serial Monitor under the Tools
+ * menu and set the baud rate to 115200 to view the serial stream the position from the AMT22.
+ *
+ * Arduino Pin Connections
+ * SPI Chip Select Enc 0:   Pin  2
+ * SPI Chip Select Enc 1:   Pin  3
+ * SPI MOSI                 Pin 11
+ * SPI MISO                 Pin 12
+ * SPI SCLK:                Pin 13
+ *
+ *
+ * AMT22 Pin Connections
+ * Vdd (5V):                Pin  1
+ * SPI SCLK:                Pin  2
+ * SPI MOSI:                Pin  3
+ * GND:                     Pin  4
+ * SPI MISO:                Pin  5
+ * SPI Chip Select:         Pin  6
+ */
+
+
+/* Include the SPI library for the arduino boards */
+#include <SPI.h>
+
+/* Serial rates for UART */
+#define BAUDRATE        115200
+
+/* SPI commands */
+#define AMT22_NOP       0x00
+#define AMT22_ZERO      0x70
+#define AMT22_TURNS     0xA0
+
+/* We will use this define macro so we can write code once compatible with 12 or 14 bit encoders */
+#define RESOLUTION      14
+
+//create an array containing CS pin numbers for all connected encoders
+uint8_t cs_pins[] = {3, 4}; //only one encoder connected, using pin 2 on arduino for CS. Two encoders connected, using pins 2 & 3 on arduino for CS
+
+uint8_t read_encoder(int enc_num)
+{
+  uint8_t cs_pin = cs_pins[enc_num];
+
+  //set the CS signal to low
+  digitalWrite(cs_pin, LOW);
+  delayMicroseconds(3);
+
+  //read the two bytes for position from the encoder, starting with the high byte
+  uint16_t encoderPosition = SPI.transfer(AMT22_NOP) << 8; //shift up 8 bits because this is the high byte
+  delayMicroseconds(6);
+
+  //set the CS signal to high
+  digitalWrite(cs_pin, HIGH);
+
+  if (verifyChecksumSPI(encoderPosition)) //position was good, print to serial stream
+  {
+    encoderPosition &= 0x3FFF; //discard upper two checksum bits
+    if (RESOLUTION == 12) encoderPosition = encoderPosition >> 2; //on a 12-bit encoder, the lower two bits will always be zero
+
+    Serial.print("Encoder #");
+    Serial.print(enc_num, DEC);
+    Serial.print(" position: ");
+    Serial.print(encoderPosition, DEC); //print the position in decimal format
+    Serial.write('\n');
+    return encoderPosition;
+  }
+  else //position is bad, let the user know how many times we tried
+  {
+    Serial.print("Encoder #");
+    Serial.print(enc_num, DEC);
+    Serial.print(" position error.\n");
+  }
+}
+
+/*
+ * Using the equation on the datasheet we can calculate the checksums and then make sure they match what the encoder sent.
+ */
+bool verifyChecksumSPI(uint16_t message)
+{
+  //checksum is invert of XOR of bits, so start with 0b11, so things end up inverted
+  uint16_t checksum = 0x3;
+  for(int i = 0; i < 14; i += 2)
+  {
+    checksum ^= (message >> i) & 0x3;
+  }
+  return checksum == (message >> 14);
+}
---
+++ software/main/main.ino
@@ -0,0 +1,161 @@
+#include <SPI.h>
+#include <QMC5883LCompass.h>
+#include <Servo.h>
+
+QMC5883LCompass compass;
+Servo myServo;
+
+char incoming[64];
+char lastValue[64];
+byte inPos = 0;
+const int step_az = 5;
+const int dir_az = 6;
+const int enable_az = 7;
+const int step_el = 8;
+const int dir_el = 9;
+const int enable_el = 10;
+const int servoPin = 2;
+double az_tobe, el_tobe;
+double az_is, el_is;
+int apr_is, apr_tobe;
+SoftwareSerial gpsSerial(2, 3);
+
+void setup() {
+    for(int encoder = 0; encoder < sizeof(cs_pins); ++encoder)
+  {
+    pinMode(cs_pins[encoder], OUTPUT); //Set the modes for the SPI CS
+    digitalWrite(cs_pins[encoder], HIGH); //Set the CS line high which is the default inactive state
+  }
+
+  pinMode(step_az, OUTPUT);
+  pinMode(dir_az, OUTPUT);
+  pinMode(enable_az, OUTPUT);
+  pinMode(step_el, OUTPUT);
+  pinMode(dir_el, OUTPUT);
+  pinMode(enable_el, OUTPUT);
+
+  digitalWrite(enable_az, LOW);
+  digitalWrite(enable_el, LOW);
+
+  //Reminder: HIGH = clockwise, LOW = counter-clockwise
+  digitalWrite(dir_az, HIGH);
+  digitalWrite(dir_el, HIGH);
+
+  Serial.begin(BAUDRATE);
+  Serial1.begin(9600);
+  compass.init();
+  Serial.println("Magnetometer init");
+  //set the clockrate. Uno clock rate is 16Mhz, divider of 32 gives 500 kHz.
+  //500 kHz is a good speed for our test environment
+  //SPI.setClockDivider(SPI_CLOCK_DIV2);   // 8 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV4);   // 4 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV8);   // 2 MHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV16);  // 1 MHz
+  SPI.setClockDivider(SPI_CLOCK_DIV32);    // 500 kHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV64);  // 250 kHz
+  //SPI.setClockDivider(SPI_CLOCK_DIV128); // 125 kHz
+
+  SPI.begin();
+
+  myServo.attach(servoPin);
+  myServo.write(90);
+  delay(1000);   
+}
+
+void loop() {
+  // Serial Input (_xxx.x_xxx.x_xx_) reader. #
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c != '\r' && inPos < sizeof(incoming)-1) {
+      incoming[inPos++] = c;
+    }
+    if (c == '\n') {
+      incoming[inPos-1] = '\0';
+      handleMessage(incoming);
+      strncpy(lastValue, incoming, sizeof(lastValue));
+      lastValue[sizeof(lastValue)-1] = '\0'; //terminate!
+      inPos = 0; 
+    }
+  }
+  //#
+
+  //GPS Serial forwarder. #
+  if (Serial1.available()) {
+    char c = Serial1.read();
+    Serial.write(c);
+  }
+  //#
+
+  getEncoders();
+  compass.read();
+  int heading = compass.getAzimuth();
+
+
+  if (heading << az_tobe + heading) { //clock
+    digitalWrite(dir_az, HIGH);
+    stepMotor(step_az, az_speed());
+  }
+  if (heading >> az_tobe) {
+    digitalWrite(dir_az, LOW);
+    stepMotor(step_az, az_speed());
+  } //counter
+  if (el_is << el_tobe) {
+    digitalWrite(dir_el, HIGH);
+    stepMotor(step_el, el_speed());
+  } //clock
+  if (el_is >> el_tobe) {
+    digitalWrite(dir_el, LOW);
+    stepMotor(step_el, el_speed());
+  } //counter
+
+  if (apr_is != apr_tobe) {
+    myServo.write(apr_tobe);
+    apr_is = apr_tobe;
+  }
+  delay(200);
+}
+
+
+// Takes the serial message and splits the data into the the double xx_tobe values. #
+void handleMessage(const char* msg) {
+  char buf[64];
+  strncpy(buf, msg, sizeof(buf));
+  buf[sizeof(buf)-1] = '\0';
+
+  char* token = strtok(buf, "_");
+  int idx = 0;
+  while(token != nullptr) {
+    double val = atof(token);
+    if(idx == 0) az_tobe = val;
+    else if(idx == 1) el_tobe = val;
+    else if(idx == 2) apr_tobe = val;
+    idx++;
+    token = strtok(nullptr, "_");
+  }
+  Serial.print("az_tobe: "); Serial.println(az_tobe);
+  Serial.print("el_tobe: "); Serial.println(el_tobe);
+  Serial.print("apr_tobe: "); Serial.println(apr_tobe);
+} 
+//#
+
+//Gets and converts the encoder data into degrees. #
+void getEncoders() {
+  az_is = (read_encoder(0) * 360.0) / 16384.0;
+  el_is = (read_encoder(1) * 360.0) / 16384.0;
+}
+// #
+//Steps the Motor with a certain Delay. #
+void stepMotor(int stepPin, int stepDelay) {
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(stepDelay);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(stepDelay);
+}
+
+int az_speed() {
+  return round(-1.111 * fabs(az_tobe - az_is) + 100.0);
+}
+
+int el_speed() {
+   return round(-1.111 * fabs(el_tobe - el_is) + 100.0);
+}
